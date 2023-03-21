45aa00a6ccd6cba172a5544be61ecf5137907ce7
Shalni555 <128455580+Shalni555@users.noreply.github.com>
2023-03-21 12:04:37 +0530

    Initial Files

---
+++ src/data.js
@@ -0,0 +1,33 @@
+import images from './img/images.jfif';
+import images_2 from './img/images_2.jpg';
+import images_1 from './img/images_1.webp';
+
+const data = {
+    productData : [
+        {
+            id:1,
+            img:images,
+            title:'Boat Shoe',
+            desc:'',
+            price:466,
+        },
+
+        {
+            id:2,
+            img:images_1,
+            title:'Bast Shoe',
+            desc:'',
+            price:556,
+        },
+
+        {
+            id:3,
+            img:images_2,
+            title:'Ballet Shoe',
+            desc:'',
+            price:669,
+        },
+    ],
+}
+
+export default data;
---
+++ src/home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Itemcart from "./itemcart";
+import data from "./data";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center mt-3">All Items</h1>
+            <section className="py-4 container">
+                <div className="row justify-content-center">
+                {data.productData.map((item , index)=>{
+                    return(
+                        <Itemcart 
+                        img={item.img}
+                        title={item.title} 
+                        desc={item.desc} 
+                        price={item.price} 
+                        item={item} 
+                        
+                        />
+ 
+                    )
+                })}
+                    
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
